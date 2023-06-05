f364cad804fbd758046bf267a50cb0ac426092b4
Bryanpham004 <bryanpham004@gmail.com>
2023-06-04 19:31:52 -0700

    Created dbfilter

--- src/DBFilter.cpp
+++ src/DBFilter.cpp
@@ -1,17 +1,38 @@
 #include "DBFilter.h"
+#include "DBManager.h"
 
 void DBFilter::queryMovieByGenre(string genre) {
-    // Implement your logic to query movies by genre
+    for (unsigned i = 0; i < MoviesByGenre.size(); i++) {
+        if (MoviesByGenre[i].getGenre() == genre) {
+            recommendedMovies.push_back(MoviesByGenre[i]);
+        }
+    }
 }
 
 void DBFilter::queryMovieByCast(string cast) {
-    // Implement your logic to query movies by cast
+    for (unsigned i = 0; i < MoviesByCast.size(); i++) {
+        if (MoviesByCast[i].getLanguage() == cast) {
+            recommendedMovies.push_back(MoviesByCast[i]);
+        }
+    }
 }
 
 void DBFilter::queryMovieByLanguage(string language) {
-    // Implement your logic to query movies by language
+    for (unsigned i = 0; i < MoviesByLanguage.size(); i++) {
+        if (MoviesByLanguage[i].getLanguage() == language) {
+            recommendedMovies.push_back(MoviesByLanguage[i]);
+        }
+    }
 }
 
 void DBFilter::queryMovieByDirector(string director) {
-    // Implement your logic to query movies by director
+    for (unsigned i = 0; i < MoviesByDirector.size(); i++) {
+        if (MoviesByDirector[i].getDirector() == director) {
+            recommendedMovies.push_back(MoviesByDirector[i]);
+        }
+    }
 }
+
+
+
+
